2d9636ff353bd7b1b05c8478f91633ca02616bf9
David Odoh <david.odoh1@gmail.com>
2022-06-10 21:04:28 +0100

    Deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
 export const environment = {
-  production: true
+  appName: 'logistic_bdt_web',
+  developmentKeyring: false,
+  production: true,
+  providerSocket: 'wss://gladios.aresprotocol.io',
+  rpc: {},
+  secretKey: '0123456789123456',
+  secretIV: '0123456789123456',
 };
