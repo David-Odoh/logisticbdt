c8b683943d29e0c34907592b1568226e7bd158ec
David Odoh <david.odoh1@gmail.com>
2022-07-03 15:02:41 +0100

    Fix base url

--- src/app/shared/services/base-url.service.ts
+++ src/app/shared/services/base-url.service.ts
@@ -5,8 +5,8 @@ import { Injectable } from '@angular/core';
 })
 export class BaseUrlService {
 
-  BASE_URL = '/api';
-  // BASE_URL = 'https://logistics-bdt-api.herokuapp.com/api';
+  // BASE_URL = '/api';
+  BASE_URL = 'https://logistics-bdt-api.herokuapp.com/api';
 
   constructor() { }
 
